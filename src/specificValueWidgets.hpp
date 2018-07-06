7e531ae1f996bec5a2d9d5b2a4ee8075f5e4ccb0
Adrian Likins <alikins@gmail.com>
2018-07-06 02:15:45 -0400

    Add missing includes

--- src/specificValueWidgets.hpp
+++ src/specificValueWidgets.hpp
@@ -1,6 +1,18 @@
-// #include "alikins.hpp"
-//#include "ui.hpp"
 #include "rack.hpp"
+
+
+
+struct SmallPurpleTrimpot : Trimpot {
+    SmallPurpleTrimpot();
+};
+
+SmallPurpleTrimpot::SmallPurpleTrimpot() : Trimpot() {
+    setSVG(SVG::load(assetPlugin(plugin, "res/SmallPurpleTrimpot.svg")));
+    shadow->blurRadius = 0.0;
+    shadow->opacity = 0.10;
+    shadow->box.pos = Vec(0.0, box.size.y * 0.1);
+}
+
 
 struct PurpleTrimpot : Trimpot {
 	Module *module;

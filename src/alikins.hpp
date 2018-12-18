740e858a64f9816f09791b0025c457d362375ca1
Adrian Likins <alikins@gmail.com>
2018-12-18 11:55:58 -0500

    Add some predefined voltage ranges

--- src/alikins.hpp
+++ src/alikins.hpp
@@ -3,6 +3,16 @@
 const int MOMENTARY_BUTTONS = 13;
 const int INPUT_SOURCES = 1;
 const int GATE_LENGTH_INPUTS = 5;
+
+enum VoltageRange {
+    ZERO_TEN,
+    MINUS_PLUS_FIVE,
+    MINUS_PLUS_TEN,
+};
+
+const float voltage_min[3] = {0.0f, -5.0f, -10.0f};
+const float voltage_max[3] = {10.0f, 5.0f, 10.0f};
+
 using namespace rack;
 
 

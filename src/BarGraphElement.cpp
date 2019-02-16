846223bdf5771c620a748eddbc08a226b59b9e24
Adrian Likins <alikins@gmail.com>
2019-02-16 15:56:40 -0500

    tabs->spaces

--- src/BarGraphElement.cpp
+++ src/BarGraphElement.cpp
@@ -3,25 +3,25 @@
 #include "alikins.hpp"
 
 struct BarGraphElement : Module {
-	enum ParamIds {
-		PITCH_PARAM,
-		NUM_PARAMS
-	};
-	enum InputIds {
-		VALUE_INPUT,
-		NUM_INPUTS
-	};
-	enum OutputIds {
-		SINE_OUTPUT,
-		NUM_OUTPUTS
-	};
-	enum LightIds {
-		BLINK_LIGHT,
-		NUM_LIGHTS
-	};
+    enum ParamIds {
+        PITCH_PARAM,
+        NUM_PARAMS
+    };
+    enum InputIds {
+        VALUE_INPUT,
+        NUM_INPUTS
+    };
+    enum OutputIds {
+        SINE_OUTPUT,
+        NUM_OUTPUTS
+    };
+    enum LightIds {
+        BLINK_LIGHT,
+        NUM_LIGHTS
+    };
 
     enum InputRange {
-		MINUS_PLUS_TEN,
+        MINUS_PLUS_TEN,
         ZERO_TEN,
         MINUS_PLUS_FIVE
     };
@@ -29,9 +29,9 @@ struct BarGraphElement : Module {
     InputRange inputRange = MINUS_PLUS_TEN;
     bool showLines = false;
 
-	float input_value = 0.0f;
+    float input_value = 0.0f;
 
-	BarGraphElement() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+    BarGraphElement() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
 
     json_t *toJson() override;
     void fromJson(json_t *rootJ) override;
@@ -60,148 +60,148 @@ void BarGraphElement::fromJson(json_t *rootJ) {
 }
 
 struct BarGraphWidget : VirtualWidget {
-	Module *module;
+    Module *module;
 
-	// guestimate of how tall the text will be
-	// TODO: can we query that?
-	float approx_text_height = 10.0f;
-	float input_value = 11.1f;
+    // guestimate of how tall the text will be
+    // TODO: can we query that?
+    float approx_text_height = 10.0f;
+    float input_value = 11.1f;
 
-	//bool showLines = false;
+    //bool showLines = false;
 
-	BarGraphWidget() {
-	};
+    BarGraphWidget() {
+    };
 
 
-	void step() override {
-		BarGraphElement *bar = dynamic_cast<BarGraphElement*>(module);
+    void step() override {
+        BarGraphElement *bar = dynamic_cast<BarGraphElement*>(module);
 
-		input_value = clamp(module->inputs[BarGraphElement::VALUE_INPUT].value,
-			voltage_min[bar->inputRange],
-			voltage_max[bar->inputRange]);
+        input_value = clamp(module->inputs[BarGraphElement::VALUE_INPUT].value,
+            voltage_min[bar->inputRange],
+            voltage_max[bar->inputRange]);
 
-		VirtualWidget::step();
-	};
+        VirtualWidget::step();
+    };
 
-	void draw(NVGcontext *vg) override {
-		// Leave some room for the text display
-		float bar_area_height = box.size.y - approx_text_height;
+    void draw(NVGcontext *vg) override {
+        // Leave some room for the text display
+        float bar_area_height = box.size.y - approx_text_height;
 
-		// debug("bar_area_height: %f, size.x: %f", bar_area_height, box.size.x);
+        // debug("bar_area_height: %f, size.x: %f", bar_area_height, box.size.x);
 
-		BarGraphElement *bar = dynamic_cast<BarGraphElement*>(module);
+        BarGraphElement *bar = dynamic_cast<BarGraphElement*>(module);
 
-		float y_origin = bar_area_height / 2.0f;
-		float bar_height_max = bar_area_height - y_origin;
-		float bar_height_min = -bar_height_max;
-		int divs = 20;
+        float y_origin = bar_area_height / 2.0f;
+        float bar_height_max = bar_area_height - y_origin;
+        float bar_height_min = -bar_height_max;
+        int divs = 20;
 
-		if (bar->inputRange == BarGraphElement::ZERO_TEN) {
-			y_origin = bar_area_height;
-			bar_height_max = bar_area_height;
-			bar_height_min = 0.0f;
+        if (bar->inputRange == BarGraphElement::ZERO_TEN) {
+            y_origin = bar_area_height;
+            bar_height_max = bar_area_height;
+            bar_height_min = 0.0f;
 
-			divs = 10;
-		}
-		if (bar->inputRange == BarGraphElement::MINUS_PLUS_FIVE) {
-			divs = 10;
-		}
+            divs = 10;
+        }
+        if (bar->inputRange == BarGraphElement::MINUS_PLUS_FIVE) {
+            divs = 10;
+        }
 
-		// debug("max_d: %d size_d_f: %f n:%d d:%d x:%d y:%d r:%f b:%f", max_d, size_d_f, n, d, x, y, red, blue);
-		nvgBeginPath(vg);
-		float bar_height = rescale(input_value, voltage_min[bar->inputRange], voltage_max		[bar->inputRange], bar_height_min, bar_height_max);
+        // debug("max_d: %d size_d_f: %f n:%d d:%d x:%d y:%d r:%f b:%f", max_d, size_d_f, n, d, x, y, red, blue);
+        nvgBeginPath(vg);
+        float bar_height = rescale(input_value, voltage_min[bar->inputRange], voltage_max		[bar->inputRange], bar_height_min, bar_height_max);
 
-		float x_middle = box.size.x / 2.0f;
+        float x_middle = box.size.x / 2.0f;
 
-		// positive values are red, negative green
-		float hue = input_value > 0 ? 0.0f : 0.35f;
+        // positive values are red, negative green
+        float hue = input_value > 0 ? 0.0f : 0.35f;
 
-		// make the bars slightly light for large values, just to add a touch of animation
-		float sat = rescale(abs(input_value), 0.0f, 10.0f, 0.75f, .6f);
-		float sat_adj = rescale(abs(input_value), 0.0f, 10.0f, 0.0, 0.05f);
-		float lightness = 0.5f;
+        // make the bars slightly light for large values, just to add a touch of animation
+        float sat = rescale(abs(input_value), 0.0f, 10.0f, 0.75f, .6f);
+        float sat_adj = rescale(abs(input_value), 0.0f, 10.0f, 0.0, 0.05f);
+        float lightness = 0.5f;
 
-		NVGcolor barColor = nvgHSL(hue, sat+sat_adj, lightness);
+        NVGcolor barColor = nvgHSL(hue, sat+sat_adj, lightness);
 
-		nvgRect(vg, 0.0f, y_origin, box.size.x, -bar_height);
+        nvgRect(vg, 0.0f, y_origin, box.size.x, -bar_height);
 
-		nvgFillColor(vg, barColor);
-		nvgFill(vg);
-		nvgStrokeColor(vg, nvgRGBAf(0.f, 0.f, 0.f, .5f));
-		nvgStroke(vg);
+        nvgFillColor(vg, barColor);
+        nvgFill(vg);
+        nvgStrokeColor(vg, nvgRGBAf(0.f, 0.f, 0.f, .5f));
+        nvgStroke(vg);
 
-		nvgFontSize(vg, 10.0f);
-		nvgBeginPath(vg);
+        nvgFontSize(vg, 10.0f);
+        nvgBeginPath(vg);
 
-		// outline the boxes a bit to make separate
-    	nvgStrokeColor(vg, nvgRGBAf(0.f, 0.f, 0.f, 1.0f));
+        // outline the boxes a bit to make separate
+        nvgStrokeColor(vg, nvgRGBAf(0.f, 0.f, 0.f, 1.0f));
 
-		nvgFillColor(vg, nvgRGBAf(0.f, 0.0f, 0.0f, 1.0f));
+        nvgFillColor(vg, nvgRGBAf(0.f, 0.0f, 0.0f, 1.0f));
 
-		// TODO: make text display optional, possible extract to method or widget
-		// snprintf(value, 100, "Velocity: %06.3fV (Midi %03d)", displayVelocity * 10.f, (int)(127 * displayVelocity));
-		std::string valueStr = stringf("%#.3g", input_value);
-    	nvgTextAlign(vg, NVG_ALIGN_CENTER|NVG_ALIGN_MIDDLE);
-    	nvgText(vg, x_middle, bar_area_height + (approx_text_height / 2.0f), valueStr.c_str(), NULL);
-		nvgStroke(vg);
-    	nvgFill(vg);
+        // TODO: make text display optional, possible extract to method or widget
+        // snprintf(value, 100, "Velocity: %06.3fV (Midi %03d)", displayVelocity * 10.f, (int)(127 * displayVelocity));
+        std::string valueStr = stringf("%#.3g", input_value);
+        nvgTextAlign(vg, NVG_ALIGN_CENTER|NVG_ALIGN_MIDDLE);
+        nvgText(vg, x_middle, bar_area_height + (approx_text_height / 2.0f), valueStr.c_str(), NULL);
+        nvgStroke(vg);
+        nvgFill(vg);
 
-		if (!bar->showLines) {
-			return;
-		}
+        if (!bar->showLines) {
+            return;
+        }
 
-		// reticular lines
-		float ret_line_centers = bar_area_height / divs;
-		debug("ret_line_centers: %f", ret_line_centers);
-		for (int i=0; i<divs; i++) {
-			nvgBeginPath(vg);
-			float y_line = 0.0f + (i*ret_line_centers);
-			debug("drawing y_line at %f", y_line);
+        // reticular lines
+        float ret_line_centers = bar_area_height / divs;
+        debug("ret_line_centers: %f", ret_line_centers);
+        for (int i=0; i<divs; i++) {
+            nvgBeginPath(vg);
+            float y_line = 0.0f + (i*ret_line_centers);
+            debug("drawing y_line at %f", y_line);
 
-			float wideness = 10.0f;
-			nvgStrokeColor(vg, nvgRGBAf(0.f, 0.f, 0.f, 0.05f));
-			if (i % 5 == 0) {
-				nvgStrokeColor(vg, nvgRGBAf(0.f, 0.f, 0.f, 0.2f));
-				wideness = 5.0f;
-			}
-			nvgRect(vg, wideness, y_line, box.size.x - (2 * wideness), 0.0f);
-			nvgFillColor(vg, nvgRGBAf(0.f, 0.f, 0.f, 0.05f));
+            float wideness = 10.0f;
+            nvgStrokeColor(vg, nvgRGBAf(0.f, 0.f, 0.f, 0.05f));
+            if (i % 5 == 0) {
+                nvgStrokeColor(vg, nvgRGBAf(0.f, 0.f, 0.f, 0.2f));
+                wideness = 5.0f;
+            }
+            nvgRect(vg, wideness, y_line, box.size.x - (2 * wideness), 0.0f);
+            nvgFillColor(vg, nvgRGBAf(0.f, 0.f, 0.f, 0.05f));
 
-			nvgStroke(vg);
-			nvgFill(vg);
-		}
-	}
+            nvgStroke(vg);
+            nvgFill(vg);
+        }
+    }
 };
 
 struct BarGraphElementWidget : ModuleWidget {
-	// another reinvented quantitywidget
-	float input_value = -13.0f;
+    // another reinvented quantitywidget
+    float input_value = -13.0f;
 
-	BarGraphWidget *barGraphWidget = NULL;
-	Menu *createContextMenu() override;
+    BarGraphWidget *barGraphWidget = NULL;
+    Menu *createContextMenu() override;
 
-	BarGraphElementWidget(BarGraphElement *module) : ModuleWidget(module) {
-		setPanel(SVG::load(assetPlugin(plugin, "res/BarGraphElement.svg")));
+    BarGraphElementWidget(BarGraphElement *module) : ModuleWidget(module) {
+        setPanel(SVG::load(assetPlugin(plugin, "res/BarGraphElement.svg")));
 
-		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+        addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+        addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		Port *input = Port::create<PJ301MPort>(Vec(33, box.size.y), Port::INPUT, module, BarGraphElement::VALUE_INPUT);
+        Port *input = Port::create<PJ301MPort>(Vec(33, box.size.y), Port::INPUT, module, BarGraphElement::VALUE_INPUT);
 
-		input->box.pos.x = 2.0f;
-		input->box.pos.y = box.size.y - input->box.size.y - 20.0f;
-		addInput(input);
+        input->box.pos.x = 2.0f;
+        input->box.pos.y = box.size.y - input->box.size.y - 20.0f;
+        addInput(input);
 
-		barGraphWidget = Widget::create<BarGraphWidget>(Vec(0.0f, 0.0f));
-		barGraphWidget->box.pos.y = box.pos.y;
-		barGraphWidget->box.pos.y = 15.0f;
+        barGraphWidget = Widget::create<BarGraphWidget>(Vec(0.0f, 0.0f));
+        barGraphWidget->box.pos.y = box.pos.y;
+        barGraphWidget->box.pos.y = 15.0f;
         barGraphWidget->box.size.x = box.size.x;
-		barGraphWidget->box.size.y = box.size.y - 60.0f;
-		barGraphWidget->module = module;
-		addChild(barGraphWidget);
-	}
+        barGraphWidget->box.size.y = box.size.y - 60.0f;
+        barGraphWidget->module = module;
+        addChild(barGraphWidget);
+    }
 };
 
 struct InputRangeItem : MenuItem {

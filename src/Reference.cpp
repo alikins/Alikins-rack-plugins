436e4ccc5e801cb4c4761a81602e9c938bd13a0a
Adrian Likins <alikins@gmail.com>
2019-06-02 22:09:15 -0400

    Mv Reference construct into struct declare

--- src/Reference.cpp
+++ src/Reference.cpp
@@ -24,8 +24,9 @@ struct Reference : Module {
         NUM_LIGHTS
     };
 
-
-    Reference() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+    Reference() {
+        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+    }
 
     void process(const ProcessArgs &args) override;
 
@@ -48,59 +49,57 @@ void Reference::process(const ProcessArgs &args) {
 }
 
 struct ReferenceWidget : ModuleWidget {
-    ReferenceWidget(Reference *module);
+    ReferenceWidget(Reference *module) {
+        setModule(module);
+        // box.size = Vec(4 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
+        setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Reference.svg")));
+
+        float y_pos = 18.0f;
+
+        float x_pos = 2.0f;
+        float y_offset = 50.0f;
+
+        addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
+                                           module,
+                                           Reference::PLUS_TEN_OUTPUT));
+
+        y_pos += y_offset;
+        addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
+                                           module,
+                                           Reference::PLUS_FIVE_OUTPUT));
+
+        y_pos += y_offset;
+        addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
+                                           module,
+                                           Reference::PLUS_ONE_OUTPUT));
+
+        y_pos += y_offset;
+        addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
+                                           module,
+                                           Reference::ZERO_OUTPUT));
+
+        y_pos += y_offset;
+        addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
+                                           module,
+                                           Reference::MINUS_ONE_OUTPUT));
+
+        y_pos += y_offset;
+        addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
+                                           module,
+                                           Reference::MINUS_FIVE_OUTPUT));
+
+        y_pos += y_offset;
+        addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
+                                           module,
+                                           Reference::MINUS_TEN_OUTPUT));
+
+
+        addChild(createWidget<ScrewSilver>(Vec(0.0f, 0.0f)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 15.0f, 0.0f)));
+        addChild(createWidget<ScrewSilver>(Vec(0.0f, 365.0f)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 15.0f, 365.0f)));
+    }
 };
 
-ReferenceWidget::ReferenceWidget(Reference *module) : ModuleWidget(module) {
-
-    // box.size = Vec(4 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
-    setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Reference.svg")));
-
-    float y_pos = 18.0f;
-
-    float x_pos = 2.0f;
-    float y_offset = 50.0f;
-
-    addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
-                                       module,
-                                       Reference::PLUS_TEN_OUTPUT));
-
-    y_pos += y_offset;
-    addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
-                                       module,
-                                       Reference::PLUS_FIVE_OUTPUT));
-
-    y_pos += y_offset;
-    addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
-                                       module,
-                                       Reference::PLUS_ONE_OUTPUT));
-
-    y_pos += y_offset;
-    addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
-                                       module,
-                                       Reference::ZERO_OUTPUT));
-
-    y_pos += y_offset;
-    addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
-                                       module,
-                                       Reference::MINUS_ONE_OUTPUT));
-
-    y_pos += y_offset;
-    addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
-                                       module,
-                                       Reference::MINUS_FIVE_OUTPUT));
-
-    y_pos += y_offset;
-    addOutput(createOutput<PJ301MPort>(Vec(x_pos, y_pos),
-                                       module,
-                                       Reference::MINUS_TEN_OUTPUT));
-
-
-    addChild(createWidget<ScrewSilver>(Vec(0.0f, 0.0f)));
-    addChild(createWidget<ScrewSilver>(Vec(box.size.x - 15.0f, 0.0f)));
-    addChild(createWidget<ScrewSilver>(Vec(0.0f, 365.0f)));
-    addChild(createWidget<ScrewSilver>(Vec(box.size.x - 15.0f, 365.0f)));
-
-}
 
 Model *modelReference = createModel<Reference, ReferenceWidget>("Reference");

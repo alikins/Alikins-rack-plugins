aaa92cc6c7e71c19d2f75d8442115ab3a5f3420e
Adrian Likins <alikins@gmail.com>
2019-06-09 22:15:05 -0400

    Use configParam

--- src/InjectValue.cpp
+++ src/InjectValue.cpp
@@ -36,7 +36,10 @@ struct InjectValue : Module
     };
 
     InjectValue() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);}
+        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+        configParam(InjectValue::INPUT_VOLTAGE_RANGE_PARAM, 0.0f, 2.0f, 0.0f, "Input Voltage Range");
+        configParam(InjectValue::INJECT_ENABLED_PARAM, 0.0f, 2.0f, 0.0f, "Enable Inject");
+    }
 
     void step() override;
 
@@ -129,7 +132,7 @@ InjectValueWidget::InjectValueWidget(InjectValue *module) : ModuleWidget(module)
 
     y_baseline = box.size.y - 128.0f;
 
-    inputVoltageSwitch = createParam<CKSSThree>(Vec(5.0f, y_baseline ), module, InjectValue::INPUT_VOLTAGE_RANGE_PARAM, 0.0f, 2.0f, 0.0f);
+    inputVoltageSwitch = createParam<CKSSThree>(Vec(5.0f, y_baseline ), module, InjectValue::INPUT_VOLTAGE_RANGE_PARAM);
 
     addParam(inputVoltageSwitch);
 
@@ -141,7 +144,7 @@ InjectValueWidget::InjectValueWidget(InjectValue *module) : ModuleWidget(module)
 
     y_baseline = box.size.y - 65.0f;
 
-    enableInjectSwitch = createParam<CKSSThree>(Vec(5, box.size.y - 62.0f), module, InjectValue::INJECT_ENABLED_PARAM, 0.0f, 2.0f, 0.0f);
+    enableInjectSwitch = createParam<CKSSThree>(Vec(5, box.size.y - 62.0f), module, InjectValue::INJECT_ENABLED_PARAM);
 
     addParam(enableInjectSwitch);
 
